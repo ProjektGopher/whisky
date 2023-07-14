b93ac047b5667a65950005d7677e4294b5f228a6
Steve Williamson <steve@sjdub.com>
2023-07-14 11:02:00 -0400

    fix tests

---
+++ app/Providers/WhiskyServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Whisky\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WhiskyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+}
