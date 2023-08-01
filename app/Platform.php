8b247192c69b01793e71d2e140cc4787109a6821
Len Woodward <Len@ProjektGopher.com>
2023-07-31 21:39:14 -0700

    add methods to Platform

--- app/Platform.php
+++ app/Platform.php
@@ -2,6 +2,8 @@
 
 namespace ProjektGopher\Whisky;
 
+use Illuminate\Support\Facades\File;
+
 class Platform
 {
     public function determineQuote(): string
@@ -12,5 +14,20 @@ class Platform
     public function isWindows(): bool
     {
         return str_starts_with(strtoupper(PHP_OS), 'WIN');
+    }
+
+    public function isNotWindows(): bool
+    {
+        return ! $this->isWindows();
+    }
+
+    public function gitIsInitialized(): bool
+    {
+        return File::exists(Whisky::cwd('.git'));
+    }
+
+    public function gitIsNotInitialized(): bool
+    {
+        return ! $this->gitIsInitialized();
     }
 }
