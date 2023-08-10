94e63a03e3eb6d76498ba0219cbfa50d29ab7ccd
Len Woodward <Len@ProjektGopher.com>
2023-08-10 02:08:22 -0700

    use Hook class in Update command

--- app/Commands/Update.php
+++ app/Commands/Update.php
@@ -2,18 +2,11 @@
 
 namespace ProjektGopher\Whisky\Commands;
 
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
-use ProjektGopher\Whisky\Platform;
-use ProjektGopher\Whisky\Whisky;
-use SplFileInfo;
+use ProjektGopher\Whisky\Hook;
 
 /**
- * TODO: This command has a lot of duplication.
- * Most of this can live on the Whisky class,
- * and some should be in a new Hook class.
+ * TODO: This has a lot of duplication wrt Commands\Uninstall and Commands\Install.
  */
 class Update extends Command
 {
@@ -23,87 +16,33 @@ class Update extends Command
 
     public function handle(): int
     {
-        $this->uninstall();
+        Hook::all(Hook::FROM_GIT)->each(function (Hook $hook): void {
+            if (! $hook->uninstall()) {
+                return;
+            }
 
-        $this->getHooks()->each(function (string $hook) {
-            $this->installHook($hook);
+            if ($this->option('verbose')) {
+                $this->info("Removed Whisky from {$hook->name} hook.");
+            }
+        });
+
+        Hook::all(Hook::FROM_CONFIG)->each(function (Hook $hook) {
+            if ($hook->fileIsMissing()) {
+                if ($this->option('verbose')) {
+                    $this->line("{$hook->name} file does not exist yet, creating...");
+                }
+                $hook->enable();
+            }
+
+            $hook->install();
+
+            if ($this->option('verbose')) {
+                $this->info("{$hook->name} git hook installed successfully.");
+            }
         });
 
         $this->line('done.');
 
         return Command::SUCCESS;
-    }
-
-    private function uninstall(): void
-    {
-        collect(
-            File::files(Platform::cwd('.git/hooks'))
-        )->filter(
-            fn (SplFileInfo $file) => ! str_ends_with($file->getFilename(), 'sample')
-        )->each(function (SplFileInfo $file): void {
-            $path = $file->getPathname();
-            $hook = $file->getFilename();
-            $contents = File::get($path);
-            $bin = Whisky::bin_path();
-            $command = "eval \"$({$bin} get-run-cmd {$hook})\"".PHP_EOL;
-
-            if (! str_contains($contents, $command)) {
-                return;
-            }
-
-            $contents = str_replace(
-                $command,
-                '',
-                File::get($path),
-            );
-            File::put($path, $contents);
-
-            if ($this->option('verbose')) {
-                $this->info("Removed Whisky from {$hook} hook.");
-            }
-        });
-    }
-
-    private function getHooks(): Collection
-    {
-        return collect(array_keys(Whisky::readConfig('hooks')));
-    }
-
-    private function hookIsInstalled(string $hook): bool
-    {
-        $bin = Whisky::bin_path();
-
-        return Str::contains(
-            File::get(Platform::cwd(".git/hooks/{$hook}")),
-            "eval \"$({$bin} get-run-cmd {$hook})\"",
-        );
-    }
-
-    private function installHook(string $hook): void
-    {
-        if (! File::exists(Platform::cwd(".git/hooks/{$hook}"))) {
-            if ($this->option('verbose')) {
-                $this->line("{$hook} file does not exist yet, creating...");
-            }
-            File::put(Platform::cwd(".git/hooks/{$hook}"), '#!/bin/sh'.PHP_EOL);
-        }
-
-        if ($this->hookIsInstalled($hook)) {
-            if ($this->option('verbose')) {
-                $this->warn("{$hook} git hook already installed, skipping...");
-            }
-
-            return;
-        }
-
-        $bin = Whisky::bin_path();
-        File::append(
-            Platform::cwd(".git/hooks/{$hook}"),
-            "eval \"$({$bin} get-run-cmd {$hook})\"".PHP_EOL,
-        );
-
-        if ($this->option('verbose')) {
-            $this->info("{$hook} git hook installed successfully.");
-        }
     }
 }
