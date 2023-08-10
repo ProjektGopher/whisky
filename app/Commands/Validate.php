09b4c4f28f32798074ef51da7f4d694083a71d3c
faissaloux <fwahabali@gmail.com>
2023-08-10 15:12:49 +0100

    validate command

---
+++ app/Commands/Validate.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ProjektGopher\Whisky\Commands;
+
+use Illuminate\Support\Facades\File;
+use LaravelZero\Framework\Commands\Command;
+use ProjektGopher\Whisky\FileJson;
+use ProjektGopher\Whisky\Platform;
+
+class Validate extends Command
+{
+    protected $signature = 'validate';
+
+    protected $description = 'Check whisky config if valid.';
+
+    public function handle(): int
+    {
+        if (File::missing(Platform::cwd('whisky.json'))) {
+            $this->error('Whisky has not been initialized in this project, aborting...');
+            $this->line('Run `./vendor/bin/whisky install` to initialize Whisky in this project.');
+
+            return Command::FAILURE;
+        }
+
+        FileJson::make(Platform::cwd('whisky.json'))->read();
+
+        $this->info('Whisky config is valid.');
+
+        return Command::SUCCESS;
+    }
+}
